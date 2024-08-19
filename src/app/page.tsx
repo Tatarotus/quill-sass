181cb220c49a41612f29522f38f3f69128530797
Samuel Baruch <shamuelbaruch@gmail.com>
2024-08-19 12:29:32 -0300

    add classname lib helper function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,5 @@ import Image from "next/image";
 import MaxWidthWrapper from "./components/MaxWidthWrapper";
 
 export default function Home() {
-  return <MaxWidthWrapper></MaxWidthWrapper>;
+  return <MaxWidthWrapper> </MaxWidthWrapper>;
 }
