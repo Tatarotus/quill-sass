6cb317f821cdd16b4b82a058b3d6fd03a60c9d70
Samuel Baruch <shamuelbaruch@gmail.com>
2024-09-06 17:55:44 -0300

    better ui uploadButton || WIP: Error on File upload

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,3 +1,4 @@
+import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
@@ -7,7 +8,7 @@ const f = createUploadthing();
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
-  pdfUploader: f({ image: { maxFileSize: "4MB" } })
+  pdfUploader: f({ pdf: { maxFileSize: "4MB" } })
     // Set permissions and file types for this FileRoute
     .middleware(() => {
       // Verify if the user is authenticated
@@ -18,12 +19,17 @@ export const ourFileRouter = {
       return { userId: user.id }; // Whatever is returned here is accessible in onUploadComplete as `metadata`
     })
     .onUploadComplete(async ({ metadata, file }) => {
-      // This code RUNS ON YOUR SERVER after upload
-      console.log("Upload complete for userId:", metadata.userId);
+      const createdFile = await db.file.create({
+        data: {
+          key: file.key,
+          name: file.name,
+          userId: metadata.userId,
+          // url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}/`,
+          url: file.url,
+          uploadStatus: "PROCESSING",
+        },
+      });
 
-      console.log("file url", file.url);
-
-      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
       return { uploadedBy: metadata.userId };
     }),
 } satisfies FileRouter;
--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
 import { Button } from "./ui/button";
 import Dropzone from "react-dropzone";
-import { Cloud, File } from "lucide-react";
+import { Cloud, File, Loader2 } from "lucide-react";
 import { Progress } from "./ui/progress";
 import { useUploadThing } from "@/app/lib/uploadthing";
 import { useToast } from "./ui/use-toast";
@@ -19,25 +19,36 @@ const UploadDropzone = () => {
 
   const { startUpload } = useUploadThing("pdfUploader");
   const { mutate: startPolling } = trpc.getFile.useMutation({
-    onSuccess: (file: any) => {
+    onSuccess: (file) => {
       router.push(`/dashboard/${file.id}`);
     },
     retry: true,
-    retryDelay: 500,
+    retryDelay: 1000,
   });
 
   const startSimulatedProgress = () => {
     setUploadProgress(0);
-
-    const interval = setInterval(() => {
+    let intervalId = setInterval(() => {
       setUploadProgress((prevProgress) => {
         if (prevProgress >= 95) {
-          clearInterval(interval);
+          if (intervalId) {
+            clearInterval(intervalId);
+          }
           return prevProgress;
         }
         return prevProgress + 5;
       });
     }, 500);
+    return intervalId;
+  };
+
+  const stopSimulatedProgress = (
+    intervalId: number | NodeJS.Timeout | null
+  ) => {
+    if (intervalId) {
+      clearInterval(intervalId);
+      intervalId = null;
+    }
   };
 
   return (
@@ -71,11 +82,10 @@ const UploadDropzone = () => {
           });
         }
 
-        clearInterval(progressInterval);
+        stopSimulatedProgress(progressInterval);
         setUploadProgress(100);
 
         startPolling({ key });
-        setIsUploading(true);
       }}
     >
       {({ getRootProps, getInputProps, acceptedFiles }) => (
@@ -110,9 +120,18 @@ const UploadDropzone = () => {
                 {isUploading ? (
                   <div className="w-full mt-4 max-w-xs-mx-auto">
                     <Progress
+                      indicatorColor={
+                        uploadProgress === 100 ? "bg-green-500" : ""
+                      }
                       value={uploadProgress}
                       className="h-1 w-full bg-zinc-200"
                     />
+                    {uploadProgress === 100 ? (
+                      <div className="flex gap-1 items center justify-center text-sm text-zinc-700 text-center pt-2">
+                        <Loader2 className="h-3 w-3 animate-spin" />
+                        Redirecting...
+                      </div>
+                    ) : null}
                   </div>
                 ) : null}
               </div>
